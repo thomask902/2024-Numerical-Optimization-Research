0305117d357d29c2d68e4c711fe400288021ad16
thomask902 <tkleinkn@uwaterloo.ca>
2024-09-18 11:48:26 -0400

    lin regression set up to take arguments

--- linear_regression.py
+++ linear_regression.py
@@ -7,6 +7,17 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from utils.gnom import GNOM
+import argparse
+import datetime
+
+# taking in arguments to determine how the model will be run
+parser = argparse.ArgumentParser(description='PyTorch Linear Regression Training with GNOM')
+
+parser.add_argument('--optimizer', default='GD', help='Choose from GD, GNOM, or GAM')
+parser.add_argument('--epochs', default=200, type=int, help='number of total epochs to run')
+parser.add_argument('--lr', default=0.01, type=float, help='initial learning rate', dest='lr')
+parser.add_argument('--gpu', default=False, type=bool, help='Set to true to train with GPU.')
+parser.add_argument('--log_base', default='./linear_regression', type=str, help='path to save logs (default: none)')
 
 # defining the model
 class linearRegression(nn.Module):
@@ -18,63 +29,86 @@ class linearRegression(nn.Module):
         out = self.linear(x)
         return out
 
-# importing the dataset
-wine_quality = fetch_ucirepo(id=186) 
-  
-X = wine_quality.data.features 
-y = wine_quality.data.targets 
+def main():
 
-#print(wine_quality.metadata) 
-#print(wine_quality.variables) 
-#print(X.head())
-#print(y.head())
+    args = parser.parse_args()
 
-# normalize features and test train split
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-X_scaled_df = pd.DataFrame(X_scaled, columns=X.columns)
+    # importing the dataset
+    wine_quality = fetch_ucirepo(id=186) 
+    
+    X = wine_quality.data.features 
+    y = wine_quality.data.targets 
 
-# Split the dataset into train and test sets (80% train, 20% test), random state fixes seed and gives same split each time
-X_train, X_test, y_train, y_test = train_test_split(X_scaled_df, y, test_size=0.2, random_state=42)
+    # normalize features and test train split
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    X_scaled_df = pd.DataFrame(X_scaled, columns=X.columns)
 
-inputDim = X_train.shape[1]
-outputDim = y_train.shape[1]
-print("Input Parameters:", inputDim)
-print("Output Parameters:", outputDim)
+    # Split the dataset into train and test sets (80% train, 20% test), random state fixes seed and gives same split each time
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled_df, y, test_size=0.2, random_state=42)
 
-# convert data into PyTorch Tensors
-X_train_vals = torch.tensor(X_train.values, dtype=torch.float32)
-X_test_vals = torch.tensor(X_test.values, dtype=torch.float32)
-y_train_vals = torch.tensor(y_train.values, dtype=torch.float32)
-y_test_vals = torch.tensor(y_test.values, dtype=torch.float32)
+    inputDim = X_train.shape[1]
+    outputDim = y_train.shape[1]
+    print("Input Parameters:", inputDim)
+    print("Output Parameters:", outputDim)
 
-# create datasets and loaders
-train_torch = torch.utils.data.TensorDataset(X_train_vals, y_train_vals)
-test_torch = torch.utils.data.TensorDataset(X_test_vals, y_test_vals)
-train_loader = torch.utils.data.DataLoader(dataset=train_torch, batch_size=len(train_torch), shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=test_torch, batch_size=len(test_torch), shuffle=False)
+    # convert data into PyTorch Tensors
+    X_train_vals = torch.tensor(X_train.values, dtype=torch.float32)
+    X_test_vals = torch.tensor(X_test.values, dtype=torch.float32)
+    y_train_vals = torch.tensor(y_train.values, dtype=torch.float32)
+    y_test_vals = torch.tensor(y_test.values, dtype=torch.float32)
 
-# set up model
-learningRate = 0.01 
-epochs = 200
-model = linearRegression(inputDim, outputDim)
-model2 = linearRegression(inputDim, outputDim)
+    # create datasets and loaders
+    train_torch = torch.utils.data.TensorDataset(X_train_vals, y_train_vals)
+    test_torch = torch.utils.data.TensorDataset(X_test_vals, y_test_vals)
+    train_loader = torch.utils.data.DataLoader(dataset=train_torch, batch_size=len(train_torch), shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_torch, batch_size=len(test_torch), shuffle=False)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
+    # set up model
+    learningRate = args.lr
+    epochs = args.epochs
+    model = linearRegression(inputDim, outputDim)
 
-# initialize loss and optimzer
-criterion = torch.nn.MSELoss() 
-base_optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
-optimizer = GNOM(params=model.parameters(), base_optimizer=base_optimizer, model=model)
+    device = torch.device('cuda' if args.gpu else 'cpu')
+    model.to(device)
 
-# training
-print("Training with GNOM")
+    # initialize loss and optimzer
+    criterion = torch.nn.MSELoss() 
+    base_optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
 
-test_accuracy = []
+    if args.optimizer == "GD":
+        optimizer = base_optimizer
+    elif args.optimizer == "GNOM":
+        optimizer = GNOM(params=model.parameters(), base_optimizer=base_optimizer, model=model)
+    else:
+        print("Please enter a valid optimizer")
+        return # exits program
 
-for epoch in range(epochs):
+    # training
+    print(f"Training with {args.optimizer}") # this will be in name of output file
+
+    test_accuracy = []
+
+    for epoch in range(epochs):
+        # train model for epoch
+        if args.optimizer == "GD":
+            train_loss, train_grad_norm, train_time = train_epoch_base(model, optimizer, train_loader, device, criterion)
+        else:
+            train_loss, train_grad_norm, train_time = train_epoch_closure(model, optimizer, train_loader, device, criterion)
+
+        # evaluate model after training
+        test_loss, test_grad_norm = evaluate(model, test_loader, criterion, device)
+        
+        # output epoch results
+        print(f'Epoch {epoch+1}, Training Loss: {train_loss}, Test Loss: {test_loss}')
+
+def train_epoch_closure(model, optimizer, train_loader, device, criterion):
     model.train()  # Set model to training mode
+
+    # TO SET UP
+    grad_norm = 0
+    time = 0
+
     total_loss = 0
     for batch_idx, (inputs, labels) in enumerate(train_loader):
         inputs, labels = inputs.to(device), labels.to(device)
@@ -89,67 +123,56 @@ for epoch in range(epochs):
         
         total_loss += loss.item()
 
-    # test set eval
-    model.eval()  # Set model to evaluation mode
-    with torch.no_grad():
-        test_loss = 0
-        for test_inputs, test_labels in test_loader:
-            test_inputs, test_labels = test_inputs.to(device), test_labels.to(device)
+    train_loss = total_loss/len(train_loader)
 
-            test_outputs = model(test_inputs)
-            test_loss += criterion(test_outputs, test_labels).item()
+    return train_loss, grad_norm, time
 
-        mse = test_loss / len(test_loader)
-        test_accuracy.append(mse)
+def train_epoch_base(model, optimizer, train_loader, device, criterion):
+    model.train()  # Set model to training mode
+
+    # TO SET UP
+    grad_norm = 0
+    time = 0
     
-    print(f'Epoch {epoch+1}/{epochs}, Training Loss: {total_loss/len(train_loader)}, Test Loss: {mse}')
-
-print("Training with SGD")
-model2.to(device)
-sgd_optimizer = torch.optim.SGD(model2.parameters(), lr=learningRate)
-test_accuracy2 = []
-
-for epoch in range(epochs):
-    model2.train()  # Set model to training mode
     total_loss = 0
     for batch_idx, (inputs, labels) in enumerate(train_loader):
         inputs, labels = inputs.to(device), labels.to(device)
         
-        # Zero the gradients
-        sgd_optimizer.zero_grad()
+        optimizer.zero_grad()
 
         # Forward pass
-        outputs = model2(inputs)
+        outputs = model(inputs)
 
         # Compute loss
         loss = criterion(outputs, labels)
 
         # Backward pass and optimization
         loss.backward()
-        sgd_optimizer.step()
-
+        optimizer.step()
+        
         total_loss += loss.item()
 
-    # test set eval
-    model2.eval()  # Set model to evaluation mode
+    train_loss = total_loss/len(train_loader)
+
+    return train_loss, grad_norm, time
+
+def evaluate(model, test_loader, criterion, device):
+    model.eval()  # Set model to evaluation mode
+
+    # TO SET UP
+    grad_norm = 0
+
     with torch.no_grad():
-        test_loss = 0
+        total_loss = 0
         for test_inputs, test_labels in test_loader:
             test_inputs, test_labels = test_inputs.to(device), test_labels.to(device)
 
-            test_outputs = model2(test_inputs)
-            test_loss += criterion(test_outputs, test_labels).item()
+            test_outputs = model(test_inputs)
+            total_loss += criterion(test_outputs, test_labels).item()
 
-        mse = test_loss / len(test_loader)
-        test_accuracy2.append(mse)
+        test_loss = total_loss / len(test_loader)
     
-    print(f'Epoch {epoch+1}/{epochs}, Training Loss: {total_loss/len(train_loader)}, Test Loss: {mse}')
+    return test_loss, grad_norm
 
-# plotting the training 
-plt.plot(range(1, epochs + 1), test_accuracy, label='GNOM')
-plt.plot(range(1, epochs + 1), test_accuracy2, label='SGD')
-plt.xlabel('Epoch')
-plt.ylabel('Test Loss (MSE)')
-plt.title(f'Test Loss over {epochs} Epochs')
-plt.legend()
-plt.show()
+if __name__ == '__main__':
+    main()
