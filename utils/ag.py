145e503c301341ce4d936101dd5694bf81d94329
thomask902 <tkleinkn@uwaterloo.ca>
2024-10-17 10:41:08 -0400

    accel grad hopefully done :)

--- utils/ag.py
+++ utils/ag.py
@@ -2,22 +2,116 @@ import torch
 
 class AG(torch.optim.Optimizer):
 
-    def __init__(self, params, base_optimizer, model, args=None):
+    def __init__(self, params, base_optimizer, model, loss_type, lipschitz, args=None):
         defaults = dict()
         super(AG, self).__init__(params, defaults)
 
         self.model = model
         self.base_optimizer = base_optimizer
-        self.args = args
-        self.convexity = True # default
-        if(self.args.loss == "hinge"):
-            self.convexity = True
-        elif(self.args.loss == "sigmoid"):
-            self.convexity = False
 
-    def step(self):
-        # add code here to step
-        return ""
+        # loss
+        self.loss_type = loss_type
+        self.convex = True # default
+        if(self.loss_type == "hinge"):
+            self.convex = True
+        elif(self.loss_type == "sigmoid"):
+            self.convex = False
+
+        # initial parameter values
+        self.lipschitz = lipschitz
+        self.beta_k = 1.0 / (2.0 * self.lipschitz)
+        self.alpha_k = 1.0
+        if self.convex:
+            self.lambda_k = self.beta_k / 2.0
+        else:
+            self.lambda_k = self.beta_k
+
+        # other parameters if needed
+        self.args = args
+
+    def update_k(self):
+        # update alpha
+        current_alpha = self.alpha_k
+        self.alpha_k = (2.0 * current_alpha) / (2 + current_alpha)
+
+        # update lambda if needed
+        if self.convex:
+            current_lambda = self.lambda_k
+            self.lambda_k = (2.0 * current_lambda + self.beta_k) / 2.0
+
+    # closure re-evaluates the function and returns loss, used in algos with multiple evaluations of objective function
+    def set_closure(self, loss_fn, inputs, targets, create_graph=True, **kwargs):
+
+        def get_grad():
+            self.zero_grad()
+            outputs = self.model(inputs)
+            loss = loss_fn(outputs, targets, **kwargs)
+            loss_value = loss.data.clone().detach()
+            loss.backward(create_graph = create_graph)
+            return outputs, loss_value
+
+        self.forward_backward_func = get_grad
+
+    # uses closure as argument, calling it internally multiple times as needed, this is why closure is used
+    def step(self, closure=None):
+
+        if closure:
+            get_grad = closure
+        else:
+            get_grad = self.forward_backward_func
+
+
+        # initialize state if not already initialized, this will store our actual and accelerated information
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                if len(state) == 0:
+                    # State initialization
+                    state['x_k'] = p.data.clone()  # x_k-1
+                    state['x_ag_k'] = p.data.clone()  # x^ag_k-1
+
+        # retreive x_k-1 and x^ag_k-1 for each parameter
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                x_k_minus1 = state['x_k']
+                x_ag_k_minus1 = state['x_ag_k']
+
+                # compute x_md_k = (1 - alpha_k) * x^ag_k-1 + alpha_k * x_k-1
+                x_md_k = (1 - self.alpha_k) * x_ag_k_minus1 + self.alpha_k * x_k_minus1
+                state['x_md_k'] = x_md_k.clone()
+
+                # set parameter p to x_md_k for gradient computation
+                p.data.copy_(x_md_k)
+
+
+        # compute gradient at x^md_k (get_grad)
+        outputs, loss_value = get_grad()
+
+        # now need to update x_k and x^md_k
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                x_k_minus1 = state['x_k']
+                x_ag_k_minus1 = state['x_ag_k']
+                x_md_k = state['x_md_k']
+                grad = p.grad.data
+
+                # update x_k and x_ag_k
+                x_k = x_k_minus1 - self.lambda_k * grad
+                x_ag_k = x_ag_k_minus1 - self.beta_k * grad
+
+                # update state
+                state['x_k'] = x_k.clone()
+                state['x_ag_k'] = x_ag_k.clone()
+
+                # set model paramters to x_k
+                p.data.copy_(x_k)
+
+        # set "k = k+1" by updating parameters (last step)
+        self.update_k()
+
+        return loss_value
 
 
     def __repr__(self):
@@ -25,6 +119,6 @@ class AG(torch.optim.Optimizer):
             f"Model: {repr(self.model)} \n"
             f"Base Optimizer: {repr(self.base_optimizer)} \n"
             f"Loss Function: {self.args.loss} \n"
-            f"Loss function convex? {self.convexity} \n"
+            f"Loss function convex: {self.convexity} \n"
             f"Lipschitz Constant of Loss Function: {self.args.lipschitz}"
         )
